de4b9ffdbe7266caf410c22adaad5c58c6215434
Chad Lohrli <chadlohrli@Chads-MacBook-Pro.local>
2017-02-07 15:36:03 -0800

    Added basic parsing functionality Updated UI to display parsed results

--- SoilMoistureSensorApp/ViewController.swift
+++ SoilMoistureSensorApp/ViewController.swift
@@ -9,6 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //raw data variables
+    @IBOutlet weak var adc: UILabel!
+    @IBOutlet weak var resistance: UILabel!
+    @IBOutlet weak var voltage: UILabel!
+    @IBOutlet weak var temperature: UILabel!
+    
+    let sparkfunURL:String = "http://192.168.4.1/read" //chip's URL
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +28,55 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func refresh(_ sender: Any) {
+        
+        parseData()
+        
+    }
+    
+    func parseData(){
+        
+        //TODO: implement a check to see if we are connected to sensor
+        
+        
+        let url = URL(string: sparkfunURL) //url instantiation
+        let data = try? Data(contentsOf: url!) //data instantiation
+        var jsonResult: [String:AnyObject] = [:] //declare json variable 
+        
+        do {
+            
+            if(data != nil) {
+                
+                //parse json data
+                jsonResult = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.allowFragments) as! [String:AnyObject]
+                
+                
+                //if values are not nil, set respective values
+                if let adcValue = jsonResult["adcValue"] {
+                    adc.text = adcValue.stringValue
+                }
+                
+                if let resistanceValue = jsonResult["resistance"] {
+                    resistance.text = resistanceValue.stringValue
+                }
+                if let voltageValue = jsonResult["voltage"] {
+                    voltage.text = voltageValue.stringValue
+                }
+                
+                if let temperatureValue = jsonResult["temperature"] {
+                    temperature.text = temperatureValue.stringValue
+                }
+            
+                
+            }else{
+                adc.text = "No Data Recieved"
+            }
+            
+        }catch let error as NSError {
+            print(error)
+        }
+        
+    }
 
 }
 
